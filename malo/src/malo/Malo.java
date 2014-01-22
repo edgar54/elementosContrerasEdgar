593d591b5b6d49f5c232cba6a9cd34c3deb1b35b
Edgar Contreras Mandujano <edgarcontreras54@yahoo.com.mx>
2014-01-21 21:30:08 -0600

    Update Malo.java

--- malo/src/malo/Malo.java
+++ malo/src/malo/Malo.java
@@ -7,7 +7,7 @@ public class Malo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-  System.out.println("Mi primer proyecto  :(");
+  System.out.println("Mi primer proyecto :(");
     }
     
 }
